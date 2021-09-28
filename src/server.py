150e66acdf7b0c022efe257040681ddfbbfc87cd
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-09-27 21:00:37 -0400

    feat: add proj skeleton

---
+++ src/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from blueprints import UserBP, DrinkBP, ReviewBP
+
+# init flask
+app = Flask(__name__)
+
+# register blueprints
+app.register_blueprint(UserBP.api)
+app.register_blueprint(DrinkBP.api)
+app.register_blueprint(ReviewBP.api)
+
+if __name__ == '__main__':
+    app.run(debug = True)
