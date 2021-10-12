830a47817dcf8743835e77f53a52e2df5de87663
JW <wenroujing@hotmail.com>
2021-10-12 19:40:56 -0400

    add repr function on review model

--- src/models/Review.py
+++ src/models/Review.py
@@ -1,5 +1,6 @@
 #temp
 from bson import ObjectId
+from pprint import pformat
 import datetime
 
 class Review:
@@ -18,6 +19,10 @@ class Review:
     self.drink_id = drink_id
     self.comment = comment
     self.rating = rating
+
+    def __repr__(self) -> str:
+      data = pformat(vars(self))[1:-1]
+      return f"Review <\n {data}\n>"
 
   #need to come back to this funciton
   def toJSON(self) -> dict[str, str]:
