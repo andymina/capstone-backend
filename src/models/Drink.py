0966378371ed7dcd1b5db325ae1eef5f9e5db0dd
JW <wenroujing@hotmail.com>
2021-10-12 23:15:14 -0400

    add a set to store reviews attached to drink

--- src/models/Drink.py
+++ src/models/Drink.py
@@ -4,9 +4,12 @@ from pprint import pformat
 class Drink:
 
   def __init__(self, user_id: ObjectId, ingredients = [ ]) -> None :
-    self._id: ObjectId = None
     self.user_id = user_id
     self.ingredients = ingredients
+
+    # set _id fields
+    self._id: ObjectId = None
+    self.review_ids = set()  #ObjectIds of reviews on this drink
 
   def __repr__(self) -> str:
     data = pformat(vars(self))[1:-1]
