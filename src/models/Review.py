c360b1838df74becfd7c1d67a5e4a4faa090ac7c
JW <wenroujing@hotmail.com>
2021-10-12 20:17:03 -0400

    remove redundent class attribute, clean up toJASON function

--- src/models/Review.py
+++ src/models/Review.py
@@ -4,37 +4,30 @@ from pprint import pformat
 import datetime
 
 class Review:
-  _id: ObjectId
-  user_id: str
-  drink_id: str
-  comment: str # make the comment to be one singualr string
-  rating : str
-  date = datetime.datetime.now()
 
-  def __init__(self, user_id: str, drink_id: str, comment: str, rating: str) -> None:
+  def __init__(self, user_id: str, drink_id: str, comment: str, rating: int) -> None:
     """
     Create a Review object according to our system diagram.
     """
+    self._id: ObjectId = None
+
     self.user_id = user_id
     self.drink_id = drink_id
     self.comment = comment
     self.rating = rating
+    self.date = datetime.datetime.now()
 
     def __repr__(self) -> str:
       data = pformat(vars(self))[1:-1]
       return f"Review <\n {data}\n>"
 
   #need to come back to this funciton
-  def toJSON(self) -> dict[str, str]:
+  def toJSON(self) -> dict:
 
     res = vars(self)
     res['user_id'] = str(self.user_id)
     res['drink_id'] = str(self.drink_id)
-    res['date'] = self.date
-
-    # assure that every val is a str
-    for key in res.keys():
-      res[key] = str(res[key])
+    res['date'] = str(self.date)
 
     return res
 
