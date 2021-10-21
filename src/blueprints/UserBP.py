cf1db23f32d734354957320dcbb1f81b5481b50c
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-10-20 21:33:56 -0400

    deploy: add Procfile

--- src/blueprints/UserBP.py
+++ src/blueprints/UserBP.py
@@ -1,8 +1,11 @@
+from logging import Logger
+import logging
 from flask import Blueprint
 from db.driver import DBdriver
+from logging import getLogger
 
 api = Blueprint('UserAPI', __name__)
-db = DBdriver()
+db = DBdriver(getLogger())
 
 @api.route("/user")
 def hello_world():
--- src/main.py
+++ src/main.py
@@ -18,4 +18,4 @@ app.register_blueprint(DrinkBP.api)
 app.register_blueprint(ReviewBP.api)
 
 if __name__ == "__main__":
-  app.run(debug = True)
+  app.run(threaded = True, debug = True)
