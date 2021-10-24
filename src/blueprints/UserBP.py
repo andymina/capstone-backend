7a8a1edc2a00f784e908186ceb00401c7279144a
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-10-24 19:00:09 -0400

    refactor: test different CORS resolution

--- src/blueprints/UserBP.py
+++ src/blueprints/UserBP.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
-from flask_cors.decorator import cross_origin
+from flask_cors import CORS, cross_origin
 from db.driver import DBdriver
 from logging import getLogger
 
 api = Blueprint('UserAPI', __name__)
-db = DBdriver(getLogger())
+CORS(api)
+db = DBdriver(getLogger(__name__))
 
 @api.route("/user")
-@cross_origin()
 def hello_world():
     return { "data": "Hello from UserAPI" }
