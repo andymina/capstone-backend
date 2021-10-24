0dee96e9c69775d9cab11fd439d2078a6151f7e4
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-10-24 18:34:25 -0400

    feat: seed the db

--- src/db/driver.py
+++ src/db/driver.py
@@ -29,24 +29,6 @@ class DBdriver:
     }
 
   # region User
-  def toUser(self, doc: dict) -> User:
-    """Converts a MongoDB document to a User.
-
-      Arguments:
-        - doc { dict }: a document representing a User
-
-      Returns:
-        - `User`
-    """
-    # create the obj
-    res = User(doc['fname'], doc['lname'], doc['email'], doc['pw'])
-    # set User _id
-    res._id = doc['_id']
-    # set the containers of _ids
-    for type in User.types:
-      setattr(res, type + '_ids', set(doc[type + '_ids']))
-
-    return res
 
   def getUser(self, email: str) -> User or None:
     """Gets a User by email.
@@ -85,64 +67,6 @@ class DBdriver:
     # create in db and return
     temp._id = self.client.users.insert_one(doc).inserted_id
     return temp
-
-  def attachItem(self, type: str, email: str, _id: ObjectId, hint: User = None):
-    """Attach an item to the User given the user's email and item's _id.
-
-      Arguments:
-        - type { str }: Must be one of ['drink', 'favorite', 'review'].
-        - email { str }
-        - _id { ObjectId }
-        - hint { User, optional }: Providing a User for hint will prevent getting the User
-          from the database. Defaults to None.
-
-      Raises:
-         - `KeyError`: raised if User with the given email (email param or hint.email) DNE.
-    """
-    # grab the user
-    target = hint if hint is not None else self.getUser(email)
-    if target is None:
-      raise KeyError(f"User with email `{target.email}` DNE")
-
-    # attempt to update db
-    res = self.client.users.update_one(
-      { 'email': target.email },
-      { '$addToSet': { f"{type}_ids": _id } }
-    )
-    # check if update failed
-    if res.modified_count == 0:
-      raise KeyError(f"User with email `{target.email}` DNE")
-
-    target.add_item(type, _id)
-
-  def detachItem(self, type: str, email: str, _id: ObjectId, hint: User = None):
-    """Optimized way to detach _id from user's drinks, favorites, or reviews.
-
-      Arguments:
-        - type { str }: Must be one of ['drink', 'favorite', 'review']
-        - email { str }
-        - _id { ObjectId }
-        - hint { User, optional }: Providing a User for hint will prevent getting the User
-          from the database. Defaults to None.
-
-      Raises:
-         - `KeyError`: raised if User with the given email (email param or hint.email) DNE.
-    """
-    # grab the user
-    target = hint if hint is not None else self.getUser(email)
-    if target is None:
-      raise KeyError(f"User with email `{target.email}` DNE")
-
-    # attempt to update db
-    res = self.client.users.update_one(
-      { 'email': target.email },
-      { '$pullAll': { f"{type}_ids": _id } }
-    )
-    # check if update failed
-    if res.modified_count == 0:
-      raise KeyError(f"User with email `{target.email}` DNE")
-
-    target.remove_item(type, _id)
 
   def getItems(self, type: str, email: str) -> list:
     """Returns all items of 'type' created by this user.
@@ -209,24 +133,6 @@ class DBdriver:
   # endregion
   
   # region Review
-  def toReview(self, doc: dict) -> Review:
-    """Converts a MongoDB document to a Review.
-
-      Arguments:
-        - doc { dict }: a document representing a Review
-
-      Returns:
-          - `Review`
-    """
-    res = Review(
-      doc['user_email'], doc['drink_id'],
-      doc['comment'], doc['rating'],
-      doc['date']
-    )
-    # set _id
-    res._id = doc['_id']
-
-    return res
   
   def getReview(self, _id: ObjectId) -> Review or None:
     """Gets a Review by _id.
@@ -250,59 +156,42 @@ class DBdriver:
         - `Review`
           - The newly created Review. If the review already exists, returns it.
     """
+    # check if the review exists
     existing_review = self.client.reviews.find_one({ 'user_email': user_email, 'drink_id': drink_id })
-    if existing_review is not None:
+    if existing_review:
       return self.toReview(existing_review)
 
+    # create the Review in the DB
     temp = Review(user_email, drink_id, comment, rating)
     temp._id = self.client.reviews.insert_one(vars(temp)).inserted_id
+
+    # attach it to a drink
+    self.attachReview(drink_id, temp._id, rating)
+    self.attachItem('review', user_email, temp._id)
     return temp
 
-  def updateReview(self, _id: ObjectId, fields: dict) -> Review or None:
-    """Updates the fields of Review by _id. If DNE, returns `None`.
-
-      Arguments:
-        - _id { ObjectId }
-        - fields { dict }: (k, v) pairs of the fields to be updated and their new values
-
-      Returns:
-        - `Review`: the updated Review.
-        - `None`: if Fields is an empty dict or Review DNE.
-    """
-    if len(fields) == 0:
-      return None
-
-    res = self.client.reviews.find_one_and_update({ '_id': _id }, { '$set': fields })
-    return self.toReview(res) if res else None
-
-  def deleteReview(self, _id: ObjectId) -> bool:
+  def deleteReview(self, review_id: ObjectId) -> bool:
     """Deletes a Review by _id in the db.
 
       Arguments:
-        - _id { ObjectId }
+        - review_id { ObjectId }
 
       Returns:
           - `bool`: True if the review was removed, False otherwise.
     """
-    res = self.client.reviews.delete_one({ '_id': _id })
-    return bool(res.deleted_count)
+    res = self.client.reviews.find_one_and_delete({ '_id': review_id })
+    if not res:
+      return False
+    
+    # update the drink
+    self.detachReview(res['drink_id'], review_id, res['rating'])
+    # update the user
+    self.detachItem('review', res['user_email'], review_id)
+    return True
+  
   # endregion
 
   # region Drink
-  def toDrink(self, doc: dict) -> Drink:
-    """Converts a MongoDB document to a Drink.
-
-      Arguments:
-        - doc { dict }: a document representing a Drink
-      
-      Returns:
-        - `Drink`
-    """
-    res = Drink(doc['user_email'], doc['name'], doc['ingredients'])
-    for k, v in doc.items():
-      setattr(res, k, v)
-    res.review_ids = set(res.review_ids)
-    return res
 
   def getDrink(self, _id: ObjectId) -> Drink or None:
     """Gets a Drink by _id.
@@ -323,8 +212,8 @@ class DBdriver:
       Returns:
         - `Drink`: The newly created Drink. If the drink already exists, returns it.
     """
-    existing_drink = self.getDrink({ 'user_email': user_email, 'name': name })
-    if existing_drink is not None:
+    existing_drink = self.client.drinks.find_one({ 'user_email': user_email, 'name': name })
+    if existing_drink:
       return existing_drink
 
     # create new Drink
@@ -332,72 +221,20 @@ class DBdriver:
     doc = vars(temp).copy()
     doc['review_ids'] = list(doc['review_ids'])
 
+    # insert drink into db
     temp._id = self.client.drinks.insert_one(doc).inserted_id
+
+    # add drink id to this user
+    self.attachItem('drink', user_email, temp._id)
     return temp
   
-  def attachReview(self, drink_id: ObjectId, review_id: ObjectId, hint: Drink = None):
-    """Attach a review to the drink specified by _id.
-
-      Arguments:
-        - drink_id { ObjectId }
-        - review_id { ObjectId }
-        - hint { Drink, optional }:Providing a User for hint will prevent getting the User
-          from the database. Defaults to None.
-
-      Raises:
-        - `KeyError`: raised if User with the given email (email param or hint.email) DNE.
-    """
-    # grab the user
-    target = hint if hint is not None else self.getDrink(drink_id)
-    if target is None:
-      raise KeyError(f"Drink with {drink_id} DNE")
-    
-    # attempt to update db
-    res = self.client.drinks.update_one(
-      { '_id': target._id },
-      { '$addToSet': { 'review_ids': review_id } }
-    )
-
-    if res.modified_count == 0:
-      raise KeyError(f"Drink with {drink_id} DNE")
-    
-    target.add_review(review_id)
-
-  def detachReview(self, drink_id: ObjectId, review_id: ObjectId, hint: Drink = None):
-    """Detaches the review with the associated _id from this drink.
-
-      Arguments:
-        - drink_id { ObjectId }
-        - review_id: { ObjectId }
-        - hint { Drink, optional }: Providing a User for hint will prevent getting the User
-          from the database. Defaults to None.
-
-      Raises:
-        - `KeyError`: raised if User with the given email (email param or hint.email) DNE.
-    """
-    # grab the user 
-    target = hint if hint is not None else self.getDrink(drink_id)
-    if target is None:
-      raise KeyError(f"Drink with drink_id {drink_id} DNE")
-
-    # attempt to update db
-    res = self.client.drinks.update_one(
-      { '_id': target._id },
-      { '$pullAll': { "review_ids": review_id } }
-    )
-    # check if update failed
-    if res.modified_count == 0:
-      raise KeyError(f"Drink with drink_id {drink_id} DNE")
-
-    target.remove_review(review_id)
-
   def getReviews(self, drink_id: ObjectId) -> list[Review]:
     """Returns a list of Reviews attached to this drink.
 
       Arguments:
         - drink_id { ObjectId }
     """
-    qres = self.client.drinks({ '_id': drink_id }, { '_id': 0, 'review_ids': 1 })
+    qres = self.client.drinks.find_one({ '_id': drink_id }, { '_id': 0, 'review_ids': 1 })
 
     if qres is None:
       raise KeyError(f"Drink with drink_id {drink_id} DNE")
@@ -425,7 +262,7 @@ class DBdriver:
     
     # attempt to update in db
     res = self.client.drinks.find_one_and_update(
-      { '_id': _id }, { '$set': fields },
+      { "_id": _id }, { "$set": fields },
       return_document = ReturnDocument.AFTER
     )
 
@@ -440,15 +277,237 @@ class DBdriver:
       Returns:
           - `bool`: True if the Drink was removed, False otherwise.
     """
-    res = self.client.drinks.delete_one({ '_id': _id })
-    return bool(res.deleted_count)
+    # delete the drink from the DB
+    res = self.client.drinks.find_one_and_delete({ "_id": _id })
+    if not res:
+      return False
+    
+    # delete the drink from the user
+    self.detachItem("drink", res["user_email"], _id)
+
+    # remove reviews attached to this drink
+    self.client.reviews.delete_many({ "_id": { "$in": res["review_ids"] } })
+    return True
 
   # endregion
 
-# region sample code
-# d = DBdriver()
+  # region internal functions
 
-# user
-# andy = d.createUser('andy', 'mina', 'andy@gmail.com', '123')
+  def toUser(self, doc: dict) -> User:
+    """Converts a MongoDB document to a User.
 
-# endregion
+      Arguments:
+        - doc { dict }: a document representing a User
+
+      Returns:
+        - `User`
+    """
+    # create the obj
+    res = User(doc['fname'], doc['lname'], doc['email'], doc['pw'])
+    # set User _id
+    res._id = doc['_id']
+    # set the containers of _ids
+    for type in User.types:
+      setattr(res, type + '_ids', set(doc[type + '_ids']))
+
+    return res
+
+  def toReview(self, doc: dict) -> Review:
+    """Converts a MongoDB document to a Review.
+
+      Arguments:
+        - doc { dict }: a document representing a Review
+
+      Returns:
+          - `Review`
+    """
+    res = Review(
+      doc['user_email'], doc['drink_id'],
+      doc['comment'], doc['rating'],
+      doc['date']
+    )
+    # set _id
+    res._id = doc['_id']
+
+    return res
+
+  def toDrink(self, doc: dict) -> Drink:
+    """Converts a MongoDB document to a Drink.
+
+      Arguments:
+        - doc { dict }: a document representing a Drink
+      
+      Returns:
+        - `Drink`
+    """
+    res = Drink(doc['user_email'], doc['name'], doc['ingredients'])
+    for k, v in doc.items():
+      setattr(res, k, v)
+    res.review_ids = set(res.review_ids)
+    return res
+
+  def attachReview(
+    self, drink_id: ObjectId, review_id: ObjectId, rating: int
+  ):
+    """Attach a review to the drink specified by _id.
+
+      Arguments:
+        - drink_id { ObjectId }
+        - review_id { ObjectId }
+        - drink_hint { Drink, optional }: Providing a Drink for hint will prevent
+          getting the Drink from the database. Defaults to None.
+        - review_hint { Review, optional }: Providing a Review for hint will prevent
+          getting the Review from the database. Defaults to None.
+
+      Raises:
+        - `KeyError`: raised if Drink with the given _id DNE.
+    """
+    # attempt to update db
+    drink = self.client.drinks.find_one_and_update(
+      { '_id': drink_id },
+      { '$addToSet': { 'review_ids': review_id } },
+      return_document = ReturnDocument.AFTER
+    )
+
+    if not drink:
+      raise KeyError(f"Drink with {drink_id} DNE")
+    
+    # update the local drink
+    drink = self.toDrink(drink)
+    drink.update_rating(rating)
+    self.updateDrink(drink_id, { 'rating': drink.rating })
+
+  def detachReview(self, drink_id: ObjectId, review_id: ObjectId, rating: int):
+    """Detaches the review with the associated _id from this drink.
+
+      Arguments:
+        - drink_id { ObjectId }
+        - review_id: { ObjectId }
+        - hint { Drink, optional }: Providing a User for hint will prevent getting the User
+          from the database. Defaults to None.
+
+      Raises:
+        - `KeyError`: raised if User with the given email (email param or hint.email) DNE.
+    """
+    # attempt to update db
+    drink = self.client.drinks.find_one_and_update(
+      { '_id': drink_id },
+      { '$pullAll': { "review_ids": review_id } }
+    )
+
+    if not drink:
+      raise KeyError(f"Drink with drink_id {drink_id} DNE")
+
+    # update the local drink
+    drink = self.toDrink(drink)
+    drink.update_rating(-rating)
+    self.updateDrink(drink_id, { 'rating': drink.rating })
+
+  def attachItem(self, type: str, email: str, _id: ObjectId):
+    """Attach an item to the User given the user's email and item's _id.
+
+      Arguments:
+        - type { str }: Must be one of ['drink', 'favorite', 'review'].
+        - email { str }
+        - _id { ObjectId }
+
+      Raises:
+         - `KeyError`: raised if User with the given email (email param or hint.email) DNE.
+    """
+    # grab the user
+
+    # attempt to update db
+    res = self.client.users.find_one_and_update(
+      { 'email': email },
+      { '$addToSet': { f"{type}_ids": _id } }
+    )
+    # check if update failed
+    if not res:
+      raise KeyError(f"User with email `{email}` DNE")
+
+    # convert to user
+    self.toUser(res).add_item(type, _id)
+
+  def detachItem(self, type: str, email: str, _id: ObjectId):
+    """Optimized way to detach _id from user's drinks, favorites, or reviews.
+
+      Arguments:
+        - type { str }: Must be one of ['drink', 'favorite', 'review']
+        - email { str }
+        - _id { ObjectId }
+        - hint { User, optional }: Providing a User for hint will prevent getting the User
+          from the database. Defaults to None.
+
+      Raises:
+         - `KeyError`: raised if User with the given email (email param or hint.email) DNE.
+    """
+    # attempt to update db
+    res = self.client.users.find_one_and_update(
+      { 'email': email },
+      { '$pullAll': { f"{type}_ids": _id } }
+    )
+    # check if update failed
+    if not res:
+      raise KeyError(f"User with email `{email}` DNE")
+
+    # convert to user
+    self.toUser(res).remove_item(type, _id)
+
+  def seed(self) -> None:
+    # create four users
+    andy = self.createUser('andy', 'mina', 'andy@gmail.com', '123')
+    jing = self.createUser('jing', 'wen', 'jing@gmail.com', '123')
+    steph = self.createUser('steph', 'yung', 'steph@gmail.com', '123')
+    sony = self.createUser('sony', 'singh', 'sony@gmail.com', '123')
+
+    # create two drinks
+    andy_drink = self.createDrink(
+      andy.email,
+      "Andy's Drink",
+      [ ["strawberry", "4"], ["caramel", "2 pumps"], ["whipped cream", "4 oz"] ]
+    )
+
+    jing_drink = self.createDrink(
+      jing.email,
+      "Jing's Drink",
+      [ ["blueberry", "2"], ["lemonade", "16 oz"], ["iced", ""] ]
+    )
+
+    # reviews for every drink
+    self.createReview(
+      jing.email,
+      andy_drink._id,
+      "As a cold brew fan, I was excited to try it, but I also was concerned it was still going to \
+      be too sweet for my taste. Still, I am always up for a fun experiment! It’s not like a total \
+      sugar-bomb bright-pink Frappuccino concoction with extra whip.",
+      3
+    )
+
+    self.createReview(
+      steph.email,
+      andy_drink._id,
+      "I actually LOVED it — and was surprised. At first I just tasted the cold brew, no pumpkin. \
+      If your straw is fully in the cup, you don’t taste the pumpkin at all — it’s literally just \
+      classic cold brew. Then I just lifted by straw to try the foam alone, and the pumpkin cream \
+      was actually so good. REALLY. It was light and creamy, but also rich-tasting, with some good \
+      froth and a sweet pumpkin flavor that wasn’t overwhelmingly sweet",
+      5
+    )
+
+    self.createReview(
+      andy.email,
+      jing_drink._id,
+      "It's pretty similar to that Vanilla Sweet Cream Cold Brew we all know and love, but with a \
+      pumpkin cream foam instead",
+      4
+    )
+
+    self.createReview(
+      sony.email,
+      jing_drink._id,
+      "After a few minutes it kind of all swirls together. You still get that rich, strong cold \
+      brew flavor but with a soothing sweet finish",
+      2
+    )
+
+  # endregion
