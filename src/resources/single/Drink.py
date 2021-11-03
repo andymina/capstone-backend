4fa1c85e71e245545f04255e3c1d0fb21ca36963
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-11-03 00:16:41 -0400

    style: rename error_response

--- src/resources/single/Drink.py
+++ src/resources/single/Drink.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, reqparse
 class SingleDrink(Resource):
   def __init__(self) -> None:
     self.db = DBdriver()
-    self.error_response = { "data": { "res": None, "err": "Drink with that _id DNE" } }, 404
+    self.drink_dne = { "data": { "res": None, "err": "Drink with that _id DNE" } }, 404
     self.parser = reqparse.RequestParser(bundle_errors = True)
     self.parser.add_argument(
       'fields',
@@ -16,16 +16,16 @@ class SingleDrink(Resource):
   def get(self, _id: str) -> tuple[dict, int]:
     # search for _id in DBdriver
     res = self.db.getDrink(ObjectId(_id))
-    return self.error_response if not res else { "data": res.toJSON() }, 200
+    return self.drink_dne if not res else { "data": res.toJSON() }, 200
 
   def put(self, _id: str) -> tuple[dict, int]:
     args = self.parser.parse_args()
     if not args["fields"]:
       return { "data": { "err": "Missing fields parameter." } }
     res = self.db.updateDrink(ObjectId(_id), args["fields"])
-    return self.error_response if not res else { "data": res.toJSON() }, 200
+    return self.drink_dne if not res else { "data": res.toJSON() }, 200
 
   def delete(self, _id: str) -> tuple[dict, int]:
     args = self.parser.parse_args()
     res = self.db.deleteDrink(ObjectId(_id))
-    return self.error_response if not res else { "data": res }, 200
+    return self.drink_dne if not res else { "data": res }, 200
