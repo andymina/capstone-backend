124c28404585e9d17f7c404f5fb27a20dfd14731
JW <wenroujing@hotmail.com>
2021-11-04 01:22:30 -0400

    include db

--- src/resources/single/Review.py
+++ src/resources/single/Review.py
@@ -1,54 +1,42 @@
 from db.driver import DBdriver
-from flask import Flask
-from flask_restful import Resource, Api, reqparse, abort, fields
-
-app = Flask(__name__)
-api = Api(app)
-
-reviews = {
-    1: {"comment": "review1 ", "summary": "1 "},
-    2: {"comment": "review2 ", "summary": "2 "},
-    3: {"comment": "review3 ", "summary": "3 "}
-}
-
-review_post_args = reqparse.RequestParser()
-review_post_args.add_argument("summary", type=str, help="Summary is required.", required = True)
-
-review_put_args = reqparse.RequestParser()
-review_put_args.add_argument("summary", type=str)
-
-resource_fields = {
-    'id': fields.Integer,
-    'summary': fields.String
-}
+from bson import ObjectId
+from flask_restful import Resource, reqparse
 class SingleReview(Resource):
-    def get(self):
-        return reviews
-class Review(Resource):
-    def get(self, review_id):
-        return reviews[review_id]
-    def post(self, review_id):
-        args = review_post_args.parse_args()
-        if review_id in reviews:
-            abort(409, "review id already taken")
-        reviews[review_id] = {"review" : args["review"], "summary": args["summary"]}
-        return reviews['review_id']
-    def put(self, review_id):
-        args = review_put_args.parse_args()
-        if review_id not in reviews:
-            abort(404, message="summary doesn't exist, cannot update.")
-        if args['summary']:
-            reviews[review_id]['summary'] = args['summary']
-        if args['summary']:
-            reviews[review_id]['summary'] = args['summary']
-        return reviews[review_id]
+    def __init__(self) -> None:
+        self.db = DBdriver()
+        self.review_dne = {
+            "data": {
+                "res": None,
+                "err": "Review with that _id DNE"
+            }
+        }, 404
+        self.parser = reqparse.RequestParser(bundle_errors = True)
+        self.parser.add_argument('fields', type = dict)
 
-    def delete(self, review_id):
-        del reviews[review_id]
-        return reviews
+    def get(self, _id: str) -> tuple[dict, int]:
+        res = self.db.getReview(ObjectId(id))
+        return self.review_dne if not res else { "data": res.toJSON() }, 200
 
-api.add_resource(Review, '/reviews/<string:_id>')
-api.add_resource(ReviewList, '/reviews')
+    def put(self, _id: str) -> tuple[dict, int]:
+        args = self.parser.parse_args()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        if args["fields"] is None:
+            return { "data": { "err": "Missing fields parameter." }}, 400
+        elif not len(args["fields"]):
+            return { "data": { "err": "Parameter 'fields' cannot be empty." }}, 400
+
+        res = self.db.updateReview(ObjectId(_id), args["fields"])
+        return self.review_dne if not res else {
+            "data: ": {
+                "review": Review,
+                "drink_rating": Number
+            }
+        }, 200
+
+    def delete(self, _id: str) -> tuple[dict, int]:
+        res = self.db.deleteReview(ObjectId(_id))
+        return self.review_dne if not res else { "data":{
+            "deleted": String,
+            "drink_rating": Number
+            }
+        }, 200
