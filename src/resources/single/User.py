2020705222247541bbc854c3058238ca77ef9652
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-11-13 15:52:59 -0500

    fix: incorrect status code return

--- src/resources/single/User.py
+++ src/resources/single/User.py
@@ -23,12 +23,12 @@ class SingleUser(Resource):
 
   def __init__(self):
     self.db = DBdriver()
-    self.user_dne = {
+    self.user_dne = ({
       "data": {
         "res": None,
         "err": "User with that email DNE"
       }
-    }, 404
+    }, 404)
     self.parser = reqparse.RequestParser(bundle_errors = True)
 
   def get(self, email: str) -> tuple[dict, int]:
@@ -42,7 +42,7 @@ class SingleUser(Resource):
     """
     # grab the user
     res = self.db.getUser(email)
-    return self.user_dne if not res else { "data": res.toJSON() }, 200
+    return self.user_dne if res is None else ({ "data": res.toJSON() }, 200)
 
   def put(self, email: str) -> tuple[dict, int]:
     """Updates the user with the given email.
@@ -66,7 +66,7 @@ class SingleUser(Resource):
       return { "data": { "err": "Parameter 'fields' cannot be empty." } }, 400
 
     res = self.db.updateUser(email, args["fields"])
-    return self.user_dne if not res else { "data": res.toJSON() }, 200
+    return self.user_dne if not res else ({ "data": res.toJSON() }, 200)
 
   def delete(self, email: str) -> tuple[dict, int]:
     """Deletes the user with the given email.
@@ -78,4 +78,4 @@ class SingleUser(Resource):
           Otherwise returns the email of the deleted user.
     """
     deleted = self.db.deleteUser(email)
-    return self.user_dne if not deleted else { "data": email }, 200
+    return self.user_dne if not deleted else ({ "data": email }, 200)
