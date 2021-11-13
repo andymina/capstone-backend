f279833efa8f0bfc46e64d2e6611b6f1244dbcad
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-11-13 16:02:11 -0500

    fix: incorrect status code

--- src/resources/multiple/Review.py
+++ src/resources/multiple/Review.py
@@ -21,44 +21,43 @@ class MultipleReview(Resource):
     args = self.parser.parse_args()
 
     if args["_ids"] is not None and args["sample"] is not None:
-      return {"data": { "err": "Cannot pass both _ids and sample parameters; choose one." } }, 400
+      return ({"data": { "err": "Cannot pass both _ids and sample parameters; choose one." } }, 400)
     elif args["_ids"] is not None:
       if not len(args["_ids"]):
-        return { "data": { "err": "Parameter `_ids` cannot be empty." } }, 400
+        return ({ "data": { "err": "Parameter `_ids` cannot be empty." } }, 400)
     else:
       sample = 10 if args["sample"] is None else args["sample"]
       res = [ review.toJSON() for review in self.db.sampleReviews(sample) ]
 
-    return { "data": res }, 200
+    return ({ "data": res }, 200)
 
-    def post(self) -> tuple[dict, int]:
+  def post(self) -> tuple[dict, int]:
 
-      self.parser.add_argument('user_email', type = str)
-      self.parser.add_argument('drink_id', type = str)
-      self.parser.add_argument('comment', type = str)
-      self.parser.add_argument('rating', type = str)
+    self.parser.add_argument('user_email', type = str)
+    self.parser.add_argument('drink_id', type = str)
+    self.parser.add_argument('comment', type = str)
+    self.parser.add_argument('rating', type = str)
 
-      args = self.parser.parse_args()
-      params = (args['user_email'], args["drink_id", args["comment"]], args["rating"])
-      email, drink_id, comment, rating = params
+    args = self.parser.parse_args()
+    email, drink_id, comment, rating = params = (args['user_email'], args["drink_id"], args["comment"], args["rating"])
 
-      if None in params:
-        return { "data": { "err": "Missing one of ['email', 'drink_id', 'comment', 'rating']" } }, 400
-      if not len(comment):
-        return { "data": { "err": "Parameter `comment` cannot be empty." } }, 400
+    if None in params:
+      return ({ "data": { "err": "Missing one of ['email', 'drink_id', 'comment', 'rating']" } }, 400)
+    if not len(comment):
+      return ({ "data": { "err": "Parameter `comment` cannot be empty." } }, 400)
 
-      res = self.db.createReview(email, drink_id, comment, rating)
-      return { "data": res.toJSON() }, 200
+    res = self.db.createReview(email, drink_id, comment, rating)
+    return ({ "data": res.toJSON() }, 200)
 
-    def delete(self) -> tuple[dict, int]:
-      self.parser.add_argument("_ids", type = str, action = "append")
+  def delete(self) -> tuple[dict, int]:
+    self.parser.add_argument("_ids", type = str, action = "append")
 
-      args = self.parser.parse_args()
+    args = self.parser.parse_args()
 
-      if args["_ids"] is None:
-        return { "data": { "err": "Parameter `_ids` required." } }, 400
-      elif not len(args["_ids"]):
-        return { "data": { "err": "parameter `_ids` cannot be empty." } }, 400
+    if args["_ids"] is None:
+      return ({ "data": { "err": "Parameter `_ids` required." } }, 400)
+    elif not len(args["_ids"]):
+      return ({ "data": { "err": "parameter `_ids` cannot be empty." } }, 400)
 
-      res = [ _id for _id in args["_ids"] if self.db.deleteReview(Object_Id(_id)) ]
-      return { "data": res }, 200
+    res = [ _id for _id in args["_ids"] if self.db.deleteReview(ObjectId(_id)) ]
+    return ({ "data": res }, 200)
